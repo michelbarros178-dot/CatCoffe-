6e8965f55ea738dc49b53a978061a1035addb7c8
Michelle Betancurt <michelbarros178@gmail.com>
2026-09-03 16:36:42 -0500

    cambios

--- CrudProyecto_MOFFE/api/index.py
+++ CrudProyecto_MOFFE/api/index.py
@@ -1,12 +1,13 @@
 import os
 import sys
 
-# Agrega la carpeta contenedora (CrudProyecto_MOFFE) al path
-base_dir = os.path.dirname(os.path.dirname(__file__))
-sys.path.append(base_dir)
+# Agrega la raíz del proyecto (CrudProyecto_MOFFE) al path de Python
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, BASE_DIR)
+
+# Define la ruta del módulo settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crud.settings')
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crud.settings')
 
 app = get_wsgi_application()
