8179c37cc55138ebd3734073326e9ca5dbae288b
Michelle Betancurt <michelbarros178@gmail.com>
2026-09-03 16:10:56 -0500

    vercel

--- CrudProyecto_MOFFE/vercel.py
+++ CrudProyecto_MOFFE/vercel.py
@@ -1,4 +1,8 @@
 import os
+import sys
+
+sys.path.append(os.path.dirname(__file__))
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crud.settings')
