05e04560d422b2f95ac6a71d1e137cc008edc6bd
Michelle Betancurt <michelbarros178@gmail.com>
2026-09-03 16:22:01 -0500

    Crear la carpeta api y resombrar vercel.py a index.py

--- CrudProyecto_MOFFE/api/index.py
+++ CrudProyecto_MOFFE/api/index.py
@@ -1,7 +1,8 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(__file__))
+# Apunta al directorio padre (CrudProyecto_MOFFE)
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from django.core.wsgi import get_wsgi_application
 
